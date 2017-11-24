c7ee705dba31515ce939cb9eae72820f0de78c4d
nort3d <nort3d@yandex.ru>
2017-11-24 07:47:41 +0300

    ...

---
+++ app/src/main/java/com/example/alex_pc/testprojectformotmom/RecyclerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.alex_pc.testprojectformotmom;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+/**
+ * Created by Alex-PC on 24.11.2017.
+ */
+
+//public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+//
+//
+//
+//    public class ViewHolder extends RecyclerAdapter.ViewHolder {
+//        public int currentItem;
+//        public ImageView itemImage;
+//        public TextView itemName;
+//        public TextView itemShortDescription;
+//        public RatingBar itemRating;
+//
+//        public ViewHolder(View itemView){
+//            super(itemView);
+//            itemImage = (ImageView)itemView.get
+//        }
+//
+//    }
+//}
